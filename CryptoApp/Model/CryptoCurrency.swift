bada9d1ec7761ad5b768a9097d0b240b0abe04e4
kocakaysegul <aysegulkocak112@gmail.com>
2023-04-01 15:44:24 +0300

    CryptoCurrency model

---
+++ CryptoApp/Model/CryptoCurrency.swift
@@ -0,0 +1,16 @@
+//
+//  CryptoCurrency.swift
+//  CryptoApp
+//
+//  Created by Ayşegül Koçak on 1.04.2023.
+//
+
+import Foundation
+
+
+struct CryptoCurrency : Decodable {
+    
+    let currency : String
+    let price : String
+    
+}
