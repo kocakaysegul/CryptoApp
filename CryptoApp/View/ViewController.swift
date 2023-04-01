13ebb0f4263a878e8e27a4c4ebe9ea1bb2a842ba
kocakaysegul <aysegulkocak112@gmail.com>
2023-04-01 16:48:01 +0300

    data shown on UI

--- CryptoApp/View/ViewController.swift
+++ CryptoApp/View/ViewController.swift
@@ -10,27 +10,49 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    private var cryptoListViewModel : CryptoListViewModel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        getData()
+    }
+    
+    func getData() {
         
         let url = URL(string: "https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json")!
         Webservice().downloadCurrencies(url: url) { cryptos in
+            
             if let cryptos = cryptos {
-                print(cryptos)
+                
+                self.cryptoListViewModel = CryptoListViewModel(cryptpCurrencyList: cryptos)
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+                
             }
         }
-        
-        tableView.dataSource = self
-        tableView.delegate = self
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return self.cryptoListViewModel == nil ? 0 : self.cryptoListViewModel.numberOfRowsInSection()
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CryptoCell") as! CryptoTableViewCell
+        
+        let cryptoViewModel = self.cryptoListViewModel.cryptoAtIndex(indexPath.row)
+        
+        cell.priceText.text = cryptoViewModel.price
+        cell.currencyText.text = cryptoViewModel.name
+        
         return cell
     }
 
