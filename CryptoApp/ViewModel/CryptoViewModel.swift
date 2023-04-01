eff96cfa1eac5abbdbfd0f851cba8483e1cba912
kocakaysegul <aysegulkocak112@gmail.com>
2023-04-01 16:52:01 +0300

    extension

--- CryptoApp/ViewModel/CryptoViewModel.swift
+++ CryptoApp/ViewModel/CryptoViewModel.swift
@@ -22,6 +22,20 @@ struct CryptoListViewModel {
     
 }
 
+extension CryptoListViewModel {
+  /*
+    func numberOfRowsInSection() -> Int {
+          return self.cryptoCurrencyList.count
+      }
+      
+      func cryptoAtIndex(_ index: Int) -> CryptoViewModel {
+          let crypto = self.cryptoCurrencyList[index]
+          return CryptoViewModel(crypto)
+      }
+ */
+    
+}
+
 struct CryptoViewModel {
     
     let cryptoCurrency : CryptoCurrency
@@ -33,4 +47,26 @@ struct CryptoViewModel {
     var price : String {
         return self.cryptoCurrency.price
     }
+}
+
+
+extension CryptoViewModel {
+    /*
+     init(_ crypto: CryptoCurrency) {
+        self.crytpoCurrency = crypto
+    }
+     */
+}
+
+extension CryptoViewModel {
+    /*
+    var name: String {
+          return self.crytpoCurrency.currency
+      }
+      
+      var price: String {
+          return self.crytpoCurrency.price
+      }
+ */
+    
 }
